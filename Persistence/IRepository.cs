719c8c0204c671c5dc9fa0b0529cf3be2cd9f3fc
lexlam1524 <lamkachun1524@gmail.com>
2024-08-15 17:35:08 +1000

    Revert "upload"
    This reverts commit 14c525cb13326d4ceaa429453bde894742ddc19c.

--- Persistence/IRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Npgsql;
-using FastMember;
-namespace robot_controller_api.Persistence
-{
-    public interface IRepository
-    {
-        List<T> ExecuteReader<T>(string sqlCommand, NpgsqlParameter[] dbParams = null) where T : class, new();
-    }
-}
